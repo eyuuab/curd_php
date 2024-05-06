9f08f7e53f4c569dc3566e0dd5ff0e1e4c5a269c
eyuuab <eyobedabrham9@gmail.com>
2024-05-06 11:27:13 +0300

    create table and add header and footer

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-3xl text-center font-bold mb-4 bg-black py-6 text-white">CRUD Operation in PHP</h1>
+
+    <div class='container'>
--- index.php
+++ index.php
@@ -1,11 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include('header.php'); ?>
+<h2 class='text-xl text-center font-bold mb-4  py-6'> All Students</h2>
+<table class="table-auto max-w-screen-lg mx-auto">
+    <thead>
+        <tr>
+            <th class="px-4 py-2">ID</th>
+            <th class="px-4 py-2">First_Name</th>
+            <th class="px-4 py-2">Last_Name</th>
+            <th class="px-4 py-2">Age</th>
+            <th class="px-4 py-2">Actions</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr class='hover:bg-gray-100'>
+            <td class="border px-4 py-2">1</td>
+            <td class="border px-4 py-2">John</td>
+            <td class="border px-4 py-2">Doe</td>
+            <td class="border px-4 py-2">30</td>
+            <td class="border px-4 py-2">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+            </td>
+        </tr>
+        <tr class='hover:bg-gray-100'>
+            <td class="border px-4 py-2">2</td>
+            <td class="border px-4 py-2">Jane</td>
+            <td class="border px-4 py-2">Doe</td>
+            <td class="border px-4 py-2">28</td>
+            <td class="border px-4 py-2">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+            </td>
+        </tr>
+        <tr class='hover:bg-gray-100'>
+            <td class="border px-4 py-2">2</td>
+            <td class="border px-4 py-2">Eyobed</td>
+            <td class="border px-4 py-2">Abreham</td>
+            <td class="border px-4 py-2">28</td>
+            <td class="border px-4 py-2">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<?php include('footer.php'); ?>
